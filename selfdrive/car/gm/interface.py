bfc4119b91f4d1b96c0f1e78852ae7273d82b717
Jason Rapp <jasrapp@gmail.com>
2020-10-25 14:58:08 -0500

    Another small adjustment to gains.  Feels close, but may need to go back to tuning-master to finalize

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -156,7 +156,7 @@ class CarInterface(CarInterfaceBase):
       ret.gasMaxBP = [0.0, 5.0, 9.0, 35.0]
       ret.gasMaxV =  [0.5, 0.7, 0.9, 0.9]
       ret.longitudinalTuning.kpBP = [0.0, 5.0, 10.0, 20.0, 35.0]
-      ret.longitudinalTuning.kpV = [0.6, 0.95, 1.23, 1.33, 1.13]
+      ret.longitudinalTuning.kpV = [0.6, 0.95, 1.23, 1.30, 1.15]
       ret.longitudinalTuning.kiV = [0.5, 0.32]
     elif ret.enableGasInterceptor:
       # Use the defaults:
