58f72e2435181a5af5c91c0e0a6102f2935de95e
Jason Rapp <jasrapp@gmail.com>
2021-03-05 20:49:24 -0600

    Panda code probably needs some more work.

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -7,6 +7,15 @@
 //      accel rising edge
 //      brake rising edge
 //      brake > 0mph
+typedef struct {
+  uint32_t current_ts;
+  CAN_FIFOMailBox_TypeDef current_frame;
+  uint32_t stock_ts;
+  CAN_FIFOMailBox_TypeDef stock_frame;
+  uint32_t op_ts;
+  CAN_FIFOMailBox_TypeDef op_frame;
+  uint32_t rolling_counter;
+} gm_dual_buffer;
 
 const int GM_MAX_STEER = 300;
 const int GM_MAX_RT_DELTA = 128;          // max delta torque allowed for real time checks
